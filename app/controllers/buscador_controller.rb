bfff7dc2db2878be32d2bbfdea33356b99e2288a
Matias <matiasruizr@hotmail.es>
2019-01-29 15:20:07 -0400

    buscador con todos los parametros

--- app/controllers/buscador_controller.rb
+++ app/controllers/buscador_controller.rb
@@ -4,7 +4,7 @@ class BuscadorController < ApplicationController
     @rut = "'"+params[:rut]+"'"
     # if params[:tipo] == 'comprador'
       # @busquedalobbysta = ActiveRecord::Base.connection.execute("select * from tmp_top10licitadores where tmp_top10licitadores.adjudicacion_rut_proveedor = '#{params[:rut]}'")
-      @busquedalobbysta = ActiveRecord::Base.connection.execute("select representa_nombre, representa_rut from asistente where representa_rut = #{@rut};  ");
+    @busquedalobbysta = ActiveRecord::Base.connection.execute("select representa_nombre, representa_rut from asistente where representa_rut = #{@rut};  ");
     # end
     
     @audiencias =  ActiveRecord::Base.connection.execute("select nombres, apellidos , remunerado, count(asistente.id), cargo_activo.id  
