492b24ad4f4c3f40db9b058d2b7573a7f43b577e
Nicolas Moya Escobar <nirokun23@gmail.com>
2019-01-29 16:40:34 -0300

    Filled Index Tables
    Tablas pobladas con datos en el index

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -3,25 +3,17 @@ class IndexController < ApplicationController
 
 
   def index
-    @top10_lobbysta_proveedor = ActiveRecord::Base.connection.execute("select * from tmp_top10licitadores limit 10")
-    @topproveedoresdelestado = ActiveRecord::Base.connection.execute("select * from tmp_top10licitadores limit 10")
+       # Top 10 Lobbistas
+       @top10_lobbysta_proveedor = ActiveRecord::Base.connection.execute("select * from top_10_lobbista;")
 
-
-    # Top 10 Licitadores -> Top 10 proveedores del estado
-    @queryy =  ActiveRecord::Base.connection.execute("select count(id) as cantidad_licitaciones_adjudicadas, adjudicacion_rut_proveedor,
-                                                      adjudicacion_nombre_proveedor, sum(adjudicacion_monto_unitario * adjudicacion_antidad)
-                                                      from licitacion_item 
-                                                      group by adjudicacion_rut_proveedor 
-                                                      order by cantidad_licitaciones_adjudicadas desc
-                                                      limit 5;")
-
-    # Top 10 licitaciones -> Top 10 compradores del estado
-    @top_compradores_estado = 	ActiveRecord::Base.connection.execute("select count(id), comprador_rut_unidad, comprador_nombre_organismo, sum(adjudicacion_monto_unitario * adjudicacion_antidad)
-                                                                      from licitacion_item as li 
-                                                                      join licitacion_detalle_licitacion_item as inter on li.id = inter.licitacion_item_id
-                                                                      join licitacion_detalle as ld on ld.codigo_externo = inter.codigo_externo
-                                                                      group by ld.comprador_rut_unidad
-                                                                      order by count(id) desc
-                                                                      limit 10;")
+       #Top 10 Compradores
+       @topproveedoresdelestado = ActiveRecord::Base.connection.execute("select * from top_10_compradores;")
+   
+   
+       # Top 10 Licitadores -> Top 10 proveedores del estado
+       @queryy =  ActiveRecord::Base.connection.execute("select * from top_10_proveedores;")
+   
+       # Top 10 licitaciones -> Top 10 compradores del estado
+       @top_compradores_estado = 	ActiveRecord::Base.connection.execute("select * from top_10_organismos;")
   end 
 end
