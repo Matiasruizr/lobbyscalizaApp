87cf161b636ff5b652b95ea0661e29e11e51900c
Matias <matiasruizr@hotmail.es>
2019-01-29 16:42:35 -0400

    Condicionales buscador

--- app/controllers/buscador_controller.rb
+++ app/controllers/buscador_controller.rb
@@ -16,15 +16,21 @@ class BuscadorController < ApplicationController
     if params[:tipo]
       @tipo = "'"+params[:tipo]+"'"
     end
-    # if params[:tipo] == 'comprador'
+
+
+    if params[:tipo] == 'pasivo'
       # @busquedalobbysta = ActiveRecord::Base.connection.execute("select * from tmp_top10licitadores where tmp_top10licitadores.adjudicacion_rut_proveedor = '#{params[:rut]}'")
-    @busquedalobbysta = ActiveRecord::Base.connection.execute("select representa_nombre, representa_rut from asistente where representa_rut = #{@rut};  ");
-    # end
-    
-    @audiencias = ActiveRecord::Base.connection.execute("select nombres, apellidos, remunerado, count(asistente.id), cargo_activo.id  
+   
+    elsif 
+      params[:tipo] == 'activo'
+      @busquedalobbysta = ActiveRecord::Base.connection.execute("select representa_nombre, representa_rut from asistente where representa_rut = #{@rut};  ");
+      @audiencias = ActiveRecord::Base.connection.execute("select nombres, apellidos, remunerado, count(asistente.id), cargo_activo.id  
                                                           from asistente 
                                                           join cargo_activo on asistente.cargo_activo_id = cargo_activo.id
                                                           where representa_rut = #{@rut}
                                                           group by representa_rut;")
+    end
+    
+    
   end 
 end
