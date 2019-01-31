143896661d112ce80361f5ca4d43cd7e10500d00
Nicolas Moya Escobar <nirokun23@gmail.com>
2019-01-31 15:19:38 -0300

    Fixed  buscador

--- app/controllers/buscador_controller.rb
+++ app/controllers/buscador_controller.rb
@@ -20,7 +20,7 @@ class BuscadorController < ApplicationController
    
     elsif 
       params[:tipo] == 'activo'
-      @busquedalobbysta = ActiveRecord::Base.connection.execute("select representa_nombre, representa_rut from asistente where representa_rut = #{@rut};  ");
+      @busquedalobbysta = ActiveRecord::Base.connection.execute("select adjudicacion_nombre_proveedor , adjudicacion_rut_proveedor, FORMAT(sum(adjudicacion_monto_unitario * adjudicacion_antidad),0) from licitacion_item where adjudicacion_rut_proveedor = #{@rut};  ");
       @audiencias = ActiveRecord::Base.connection.execute("select nombres, apellidos, 
                                                           case
                                                           when remunerado = 1 then 'Si'
