2c759f47080530958b7000d7cf81bce3a87d4aaf
Matias <matiasruizr@hotmail.es>
2019-02-03 13:44:48 -0400

    Audiencia detalle actualizado con todos sus datos

--- app/controllers/audiencia_controller.rb
+++ app/controllers/audiencia_controller.rb
@@ -3,12 +3,20 @@ class AudienciaController < ApplicationController
     @id = "'"+params[:id]+"'"
 
 
-    @audiencia =  ActiveRecord::Base.connection.execute( "select nombres, apellidos, am.nombre, representa_nombre from asistente 
+    @audiencia =  ActiveRecord::Base.connection.execute( "select asistente.nombres, asistente.apellidos, am.nombre, representa_nombre,
+                                                              auca.fecha_termino,
+                                                              referencia as 'tema',
+                                                              CONCAT(spd.nombres,' ',spd.apellidos) as 'sujeto pasivo',
+                                                              spd.cargo,
+                                                              spd.institucion_nombre
+                                                          from asistente
                                                           join cargo_activo as ca on asistente.cargo_activo_id = ca.id
                                                           join audiencia_detalle_asistente as jds on asistente.id = jds.asistente_id
                                                           join audiencia_detalle as ad on jds.audiencia_detalle_id = ad.id
-                                                          join audiencia_detalle_materia as adm on  ad.id = adm.audiencia_detalle_id 
+                                                          join audiencia_detalle_materia as adm on  ad.id = adm.audiencia_detalle_id
                                                           join audiencia_materia as am on adm.audiencia_materia_id = am.id
+                                                          join audiencia_cabecera as auca on auca.id = ad.id
+                                                          join sujeto_pasivo_detalle spd on ad.sujeto_pasivo_id = spd.id
                                                           where ca.id = #{@id};")
 
         
