f807a3d26c15a45c86fd1de7a6a1acecf120be75
Matias <matiasruizr@hotmail.es>
2019-02-03 21:27:09 -0400

    database production changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+
+    @activos = ActiveRecord::Base.connection.execute( "select adjudicacion_rut_proveedor, adjudicacion_nombre_proveedor from licitacion_item
+                                                    group by adjudicacion_rut_proveedor;")
+                                                    
+
+    @pasivos = ActiveRecord::Base.connection.execute( "select comprador_rut_unidad, comprador_nombre_organismo
+                                                    from licitacion_detalle
+                                                    group by comprador_rut_unidad;")
 end
