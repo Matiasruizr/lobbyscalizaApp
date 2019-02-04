0425f73cb1fe95962ceb4bd01e46d5c46f66f2d3
Nicolas Moya Escobar <nirokun23@gmail.com>
2019-02-04 15:32:16 -0300

    Check de datos

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,4 @@ module ApplicationHelper
     def audiencias(rut)
       @audiencias  = ActiveRecord::Base.connection.execute("SELECT count(*) FROM ASISTENTE WHERE representa_rut='#{rut}';")
     end
-
-
 end
