16dd149d6515a5ba72169d8bd4a6d26a7a53e45a
Matias <matiasruizr@hotmail.es>
2019-02-06 11:50:18 -0400

    Detalle de audiencias

--- app/controllers/licitacion_controller.rb
+++ app/controllers/licitacion_controller.rb
@@ -4,14 +4,27 @@ class LicitacionController < ApplicationController
     @rut = params[:rut]
     @tipo = params[:tipo]
 
-    @licitacion_detalle =  ActiveRecord::Base.connection.execute( "select fecha_publicacion, 
-    licitacion_detalle.codigo_externo, comprador_region_unidad, comprador_nombre_unidad, 
-    comprador_rut_unidad, comprador_nombre_usuario, adjudicacion_url_acta
-   from licitacion_item
-    join licitacion_detalle_licitacion_item  as inter
-    on licitacion_item.id = inter.licitacion_item_id
-    join licitacion_detalle on inter.codigo_externo = licitacion_detalle.codigo_externo
-    where adjudicacion_rut_proveedor = '#{@rut}'
-    and tipo = '#{@tipo}';")
+    if @tipo
+      @licitacion_detalle =  ActiveRecord::Base.connection.execute( "select fecha_publicacion, 
+      licitacion_detalle.codigo_externo, comprador_region_unidad, comprador_nombre_unidad, 
+      comprador_rut_unidad, comprador_nombre_usuario, adjudicacion_url_acta
+      from licitacion_item
+      join licitacion_detalle_licitacion_item  as inter
+      on licitacion_item.id = inter.licitacion_item_id
+      join licitacion_detalle on inter.codigo_externo = licitacion_detalle.codigo_externo
+      where adjudicacion_rut_proveedor = '#{@rut}'
+      and tipo = '#{@tipo}'
+      group by licitacion_detalle.codigo_externo;")
+    else
+      @licitacion_detalle =  ActiveRecord::Base.connection.execute( "select fecha_publicacion, 
+      licitacion_detalle.codigo_externo, comprador_region_unidad, comprador_nombre_unidad, 
+      comprador_rut_unidad, comprador_nombre_usuario, adjudicacion_url_acta
+      from licitacion_item
+      join licitacion_detalle_licitacion_item  as inter
+      on licitacion_item.id = inter.licitacion_item_id
+      join licitacion_detalle on inter.codigo_externo = licitacion_detalle.codigo_externo
+      where adjudicacion_rut_proveedor = '#{@rut}';")
+
+    end
   end
 end
