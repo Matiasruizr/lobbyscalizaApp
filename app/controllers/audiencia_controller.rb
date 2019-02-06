e36b59fd8d78596d7c9917d6a7fa4cded27be9f4
Matias <matiasruizr@hotmail.es>
2019-02-06 12:22:51 -0400

    detalles audiencias y licitaciones tabla 4

--- app/controllers/audiencia_controller.rb
+++ app/controllers/audiencia_controller.rb
@@ -1,23 +1,44 @@
 class AudienciaController < ApplicationController
   def detalle 
     @id = "'"+params[:id]+"'"
+    @rut = params[:rut]
 
 
-    @licitacion_detalle =  ActiveRecord::Base.connection.execute( "select asistente.nombres, asistente.apellidos, am.nombre, representa_nombre,
-                                                              auca.fecha_termino,
-                                                              referencia as 'tema',
-                                                              CONCAT(spd.nombres,' ',spd.apellidos) as 'sujeto pasivo',
-                                                              spd.cargo,
-                                                              spd.institucion_nombre
-                                                          from asistente
-                                                          join cargo_activo as ca on asistente.cargo_activo_id = ca.id
-                                                          join audiencia_detalle_asistente as jds on asistente.id = jds.asistente_id
-                                                          join audiencia_detalle as ad on jds.audiencia_detalle_id = ad.id
-                                                          join audiencia_detalle_materia as adm on  ad.id = adm.audiencia_detalle_id
-                                                          join audiencia_materia as am on adm.audiencia_materia_id = am.id
-                                                          join audiencia_cabecera as auca on auca.id = ad.id
-                                                          join sujeto_pasivo_detalle spd on ad.sujeto_pasivo_id = spd.id
-                                                          where ca.id = #{@id};")
+
+    if @rut
+      @audiencia_detalle =  ActiveRecord::Base.connection.execute( "
+                            select asistente.nombres, asistente.apellidos, am.nombre, representa_nombre,
+                            auca.fecha_termino,
+                            referencia as 'tema',
+                            CONCAT(spd.nombres,' ',spd.apellidos) as 'sujeto pasivo',
+                            spd.cargo,
+                            spd.institucion_nombre
+                            from asistente
+                            join cargo_activo as ca on asistente.cargo_activo_id = ca.id
+                            join audiencia_detalle_asistente as jds on asistente.id = jds.asistente_id
+                            join audiencia_detalle as ad on jds.audiencia_detalle_id = ad.id
+                            join audiencia_detalle_materia as adm on  ad.id = adm.audiencia_detalle_id
+                            join audiencia_materia as am on adm.audiencia_materia_id = am.id
+                            join audiencia_cabecera as auca on auca.id = ad.id
+                            join sujeto_pasivo_detalle spd on ad.sujeto_pasivo_id = spd.id
+                            where representa_rut = '#{@rut}';")
+    else 
+      @audiencia_detalle =  ActiveRecord::Base.connection.execute( "select asistente.nombres, asistente.apellidos, am.nombre, representa_nombre,
+                                                                    auca.fecha_termino,
+                                                                    referencia as 'tema',
+                                                                    CONCAT(spd.nombres,' ',spd.apellidos) as 'sujeto pasivo',
+                                                                    spd.cargo,
+                                                                    spd.institucion_nombre
+                                                                    from asistente
+                                                                    join cargo_activo as ca on asistente.cargo_activo_id = ca.id
+                                                                    join audiencia_detalle_asistente as jds on asistente.id = jds.asistente_id
+                                                                    join audiencia_detalle as ad on jds.audiencia_detalle_id = ad.id
+                                                                    join audiencia_detalle_materia as adm on  ad.id = adm.audiencia_detalle_id
+                                                                    join audiencia_materia as am on adm.audiencia_materia_id = am.id
+                                                                    join audiencia_cabecera as auca on auca.id = ad.id
+                                                                    join sujeto_pasivo_detalle spd on ad.sujeto_pasivo_id = spd.id
+                                                                    where ca.id = #{@id};")
+    end
 
      
   end
