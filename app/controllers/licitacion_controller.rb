7fad1d40e31f3c851818f920a41d7183b4ade9cb
Matias <matiasruizr@hotmail.es>
2019-02-07 13:49:29 -0400

    audiencias

--- app/controllers/licitacion_controller.rb
+++ app/controllers/licitacion_controller.rb
@@ -16,6 +16,7 @@ class LicitacionController < ApplicationController
       and tipo = '#{@tipo}'
       group by licitacion_detalle.codigo_externo;")
     else
+      @comprador = 
       @licitacion_detalle =  ActiveRecord::Base.connection.execute( "select fecha_publicacion, 
       licitacion_detalle.codigo_externo, comprador_region_unidad, comprador_nombre_unidad, 
       comprador_rut_unidad, comprador_nombre_usuario, adjudicacion_url_acta
@@ -23,7 +24,8 @@ class LicitacionController < ApplicationController
       join licitacion_detalle_licitacion_item  as inter
       on licitacion_item.id = inter.licitacion_item_id
       join licitacion_detalle on inter.codigo_externo = licitacion_detalle.codigo_externo
-      where adjudicacion_rut_proveedor = '#{@rut}';")
+      where adjudicacion_rut_proveedor = '#{@rut}'
+      and comprador_rut_unidad = '#{@comprador}';")
 
     end
   end
