7485055e4ff9837e75f87c2fa08173053fce127a
Nicolas Moya Escobar <nirokun23@gmail.com>
2019-02-15 13:25:40 -0300

    Fix Tab4 Audiencias
    Add new helper audienciasorg
    Use of this new helper on index

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,6 +12,18 @@ module ApplicationHelper
       AND comprador_nombre_organismo='#{org}'
       group by licitacion_detalle.codigo_externo)src;")
     end
+    def audienciasorg(rut,org)
+        @audienciasorg = ActiveRecord::Base.connection.execute("select count(*)
+          from asistente
+          join cargo_activo as ca on asistente.cargo_activo_id = ca.id
+          join audiencia_detalle_asistente as jds on asistente.id = jds.asistente_id
+          join audiencia_detalle as ad on jds.audiencia_detalle_id = ad.id
+          join audiencia_detalle_materia as adm on  ad.id = adm.audiencia_detalle_id
+          join audiencia_materia as am on adm.audiencia_materia_id = am.id
+          join audiencia_cabecera as auca on auca.id = ad.id
+          join sujeto_pasivo_detalle spd on ad.sujeto_pasivo_id = spd.id
+          where asistente.representa_rut = '#{rut}' and spd.institucion_nombre like '#{org}';")
+    end
     #def tabla1(rut)
     #  @tabla1 = ActiveRecord::Base.connection.execute(
     #  "select count(*) from (select count(*)
