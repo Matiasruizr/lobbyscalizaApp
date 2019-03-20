20c32d789669dbe5f6f6c28a5a2615ab784dbf08
Matias <matiasruizr@hotmail.es>
2019-03-20 20:04:28 -0300

    vistar controladores y rutas de tablas

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,18 +1,30 @@
 class IndexController < ApplicationController
 
-
-
+  def top_compradores_del_estado
+    # Top 10 licitaciones -> Top 10 compradores del estado
+    @queryy =  ActiveRecord::Base.connection.execute("select * from top_10_compradores;")
+    
+  end
+  
+  def top_lobbysta_proveedor
+    @top10_lobbysta_proveedor = ActiveRecord::Base.connection.execute("select * from top_10_lobbista LIMIT 10;")
+  end
+  
+  def top_organismos_con_mas_audiencias
+    # Top 10 Licitadores -> Top 10 proveedores del estado
+    @top_compradores_estado = 	ActiveRecord::Base.connection.execute("select * from top_10_organismos order by `count(aud.id)` desc;")
+  end
+  
+  def top_proveedores_del_estado
+    @topproveedoresdelestado = ActiveRecord::Base.connection.execute("select * from top_10_proveedores;")
+  end
+  
   def index
-      @top10_lobbysta_proveedor = ActiveRecord::Base.connection.execute("select * from top_10_lobbista LIMIT 10;")
-
-       #Top 10 Compradores
-       @topproveedoresdelestado = ActiveRecord::Base.connection.execute("select * from top_10_proveedores;")
-   
-   
-       # Top 10 Licitadores -> Top 10 proveedores del estado
-       @queryy =  ActiveRecord::Base.connection.execute("select * from top_10_compradores;")
-   
-       # Top 10 licitaciones -> Top 10 compradores del estado
-       @top_compradores_estado = 	ActiveRecord::Base.connection.execute("select * from top_10_organismos order by `count(aud.id)` desc;")
+    #Top 10 Compradores  
   end 
+
+  def quees
+       
+  end 
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,15 @@ Rails.application.routes.draw do
   get 'buscador/activo'
   get 'index/index'
 
+  get 'index/quees'
+  get 'index/top_compradores_del_estado'
+  get 'index/top_lobbysta_proveedor'
+  get 'index/top_organismos_con_mas_audiencias'
+  get 'index/top_proveedores_del_estado'
+
+
+
+
   root to: 'index#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
