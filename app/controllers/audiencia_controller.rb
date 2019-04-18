83441f044f9bc06e77a5db4707aa985413a29ac9
Nicolas Moya Escobar <n.moyae@alumnos.duoc.cl>
2019-04-18 13:50:22 -0400

    Fixed links

--- app/controllers/audiencia_controller.rb
+++ app/controllers/audiencia_controller.rb
@@ -4,6 +4,9 @@ class AudienciaController < ApplicationController
     @rut = params[:rut]
     @org = params[:nombre]
     @ruta = params[:ruta]
+    @representa = params[:representa]
+    @apellido = params[:apellido]
+    @nombre = params[:nombre]
     
    
 
@@ -44,6 +47,22 @@ class AudienciaController < ApplicationController
                           join audiencia_cabecera as auca on auca.id = ad.id
                           join sujeto_pasivo_detalle spd on ad.sujeto_pasivo_id = spd.id
                           where representa_rut = '#{@rut}';") 
+      elsif @representa
+        @audiencia_detalle = ActiveRecord::Base.connection.execute("select asistente.nombres, asistente.apellidos, am.nombre, representa_nombre,
+          auca.fecha_termino,
+          referencia as 'tema',
+          CONCAT(spd.nombres,' ',spd.apellidos) as 'sujeto pasivo',
+          spd.cargo,
+          spd.institucion_nombre
+          from asistente
+          join cargo_activo as ca on asistente.cargo_activo_id = ca.id
+          join audiencia_detalle_asistente as jds on asistente.id = jds.asistente_id
+          join audiencia_detalle as ad on jds.audiencia_detalle_id = ad.id
+          join audiencia_detalle_materia as adm on  ad.id = adm.audiencia_detalle_id
+          join audiencia_materia as am on adm.audiencia_materia_id = am.id
+          join audiencia_cabecera as auca on auca.id = ad.id
+          join sujeto_pasivo_detalle spd on ad.sujeto_pasivo_id = spd.id
+          where asistente.nombres like '#{@nombre}' and asistente.apellidos like '#{@apellido}' and representa_rut like '#{@representa}';")
       else
       @audiencia_detalle =  ActiveRecord::Base.connection.execute( "select asistente.nombres, asistente.apellidos, am.nombre, representa_nombre,
                                                                     auca.fecha_termino,
@@ -61,8 +80,6 @@ class AudienciaController < ApplicationController
                                                                     join sujeto_pasivo_detalle spd on ad.sujeto_pasivo_id = spd.id
                                                                     where ca.id = '#{@id}';")
     end
-
-     
   end
 
   def detalleaudiencia
