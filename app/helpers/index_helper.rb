0e7be4f90a9a994c96cbd3a0e9c6d540be5130a4
Nicolas Moya Escobar <n.moyae@alumnos.duoc.cl>
2019-04-22 13:57:31 -0400

    Contadores Audiencias

--- app/helpers/index_helper.rb
+++ app/helpers/index_helper.rb
@@ -11,4 +11,15 @@ module IndexHelper
         join sujeto_pasivo_detalle spd on ad.sujeto_pasivo_id = spd.id
         where spd.institucion_nombre like '#{org}';")
     end
+    def contadoraudiencias(org)
+        @contadoraudiencias = ActiveRecord::Base.connection.execute("Select count(*) from (
+            select distinct auca.fecha_inicio, auca.fecha_termino, spd.fecha_inicio as fechaini, spd.fecha_termino as fechatermi
+            from asistente
+            join cargo_activo as ca on asistente.cargo_activo_id = ca.id
+            join audiencia_detalle_asistente as jds on asistente.id = jds.asistente_id
+            join audiencia_detalle as ad on jds.audiencia_detalle_id = ad.id
+            join audiencia_cabecera as auca on auca.id = ad.id
+            join sujeto_pasivo_detalle spd on ad.sujeto_pasivo_id = spd.id
+            where  spd.institucion_nombre like '#{org}') asd;")
+    end 
 end
