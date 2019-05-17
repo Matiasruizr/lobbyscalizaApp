30ece3e2e28eb6f501f82249d75afbd0161a48ee
Raúl <ribarahonaa@gmail.com>
2019-05-17 11:23:43 -0400

    fix query 1.2

--- app/helpers/buscador_helper.rb
+++ app/helpers/buscador_helper.rb
@@ -65,9 +65,6 @@ module BuscadorHelper
         join cargo_activo as ca on asistente.cargo_activo_id = ca.id
         join audiencia_detalle_asistente as jds on asistente.id = jds.asistente_id
         join audiencia_detalle as ad on jds.audiencia_detalle_id = ad.id
-        join audiencia_detalle_materia as adm on  ad.id = adm.audiencia_detalle_id
-        join audiencia_materia as am on adm.audiencia_materia_id = am.id
-        join audiencia_cabecera as auca on auca.audience_detail_id = ad.id
         join sujeto_pasivo_detalle spd on ad.sujeto_pasivo_id = spd.id
         where asistente.representa_rut = '#{rut}' and spd.institucion_nombre like '%#{org}%';")
     end
